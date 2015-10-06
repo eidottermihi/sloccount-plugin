f623d7dc2fb24523f391dedf42c09c6cf3689421
Michael Prankl <eidottermihi@gmail.com>
2015-10-06 11:04:02 +0200

    Added dropdown in job configuration page to choose a LoC-Provider.

--- src/main/java/hudson/plugins/sloccount/SloccountLoCProvider.java
+++ src/main/java/hudson/plugins/sloccount/SloccountLoCProvider.java
@@ -3,13 +3,19 @@ package hudson.plugins.sloccount;
 import hudson.Extension;
 import hudson.model.AbstractBuild;
 import org.jenkinsci.plugins.codehealth.LinesOfCode;
+import org.jenkinsci.plugins.codehealth.LinesOfCodeDescriptor;
 import org.jenkinsci.plugins.codehealth.LinesOfCodeProvider;
+import org.jenkinsci.plugins.database.PerItemDatabaseDescriptor;
+import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * @author Michael Prankl
  */
-@Extension
 public class SloccountLoCProvider extends LinesOfCodeProvider {
+
+    @DataBoundConstructor
+    public SloccountLoCProvider() {
+    }
 
     @Override
     public LinesOfCode getLOC(AbstractBuild<?, ?> build) {
@@ -25,5 +31,20 @@ public class SloccountLoCProvider extends LinesOfCodeProvider {
     @Override
     public String getOrigin() {
         return "sloccount";
+    }
+
+    @Override
+    public LinesOfCodeDescriptor getDescriptor() {
+        return super.getDescriptor();
+    }
+
+    @Extension
+    public static class DescriptorImpl extends LinesOfCodeDescriptor {
+
+        @Override
+        public String getDisplayName() {
+            return "sloccount";
+        }
+
     }
 }
